94b8bde723075a938453b009e7363d3265abdc2f
sandra <a.egorova@innopolis.university>
2025-02-05 21:34:06 +0300

    fixed

--- app_python/app.py
+++ app_python/app.py
@@ -4,10 +4,12 @@ from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    moscow_time = datetime.utcnow().replace(tzinfo=timezone.utc) + timedelta(hours=3)
-    return render_template('index.html', time=moscow_time.strftime('%Y-%m-%d %H:%M:%S'))
+    moscow_time = (datetime.utcnow().replace(tzinfo=timezone.utc) +
+                   timedelta(hours=3))
+    return render_template("index.html",
+                           time=moscow_time.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 if __name__ == '__main__':
